3ec63e88f81a8d1692f3fd95429e42ed008841ba
Pere Serra Bosch <pereserrabosch@MacBook-Pro-de-Pere.local>
2018-05-16 17:29:27 +0200

    prepared to deploy

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class UsersService {
 
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) { }
   
