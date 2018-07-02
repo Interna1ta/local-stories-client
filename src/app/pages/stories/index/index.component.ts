8bc5416196728e23cb52973d670641f74db29c47
Pere Serra Bosch <pereserrabosch@MacBook-Pro-de-Pere.local>
2018-07-02 13:33:16 +0200

    new profile page done

--- src/app/pages/stories/index/index.component.ts
+++ src/app/pages/stories/index/index.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { StoriesService } from '../../../services/stories.service';
+import { UsersService } from '../../../services/users.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-index',
@@ -10,14 +12,35 @@ import { StoriesService } from '../../../services/stories.service';
 export class IndexComponent implements OnInit {
 
   stories: Array<any>;
+  user: any;
+  idUser: string;
 
-  constructor(private storiesService: StoriesService) { }
+  constructor(
+    private storiesService: StoriesService, 
+    private usersService: UsersService, 
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.storiesService.listAll()
       .then((data) => {
         this.stories = data;
       })
+
+    this.authService.me()
+      .then((data) => {
+        this.user = data;
+        this.idUser = data._id;
+        this.usersService.userFollowers(this.idUser)
+          .then((data) => {
+            console.log(data);
+          })
+      }) 
+
+    // this.storiesService.listTweets()
+    //   .then((data) => {
+    //     this.stories = data;
+    //   })
   }
 
 }
